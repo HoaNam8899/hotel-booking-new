ced2f3f1acd44d88ce8b73476a257de1c64cf456
HoaNam8899 <hyhoanam0069@gmail.com>
2023-05-02 00:28:01 +0700

    src

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// var mobileWidth = screen.width 
+// lấy chiều dài màn hình thực tế
+
+
+function setPlacehoder(x){
+    if (x.matches) { // If media query matches
+        $('#offer-input').attr('placeholder','Enter email...');
+      }
+    console.log(x);
+
+    
+}
+var x = window.matchMedia("(max-width: 416px)")
+setPlacehoder(x) // Call listener function at run time
+x.addListener(setPlacehoder) // Attach listener function on state changes
+
+
+
+
+// lấy chiều ngang màng hình rồi dùng để thay đổi placeholder
