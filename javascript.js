c40ebddba1c419a4a380522c97ec560030a4f562
HoaNam8899 <hyhoanam0069@gmail.com>
2023-05-02 11:58:18 +0700

    src

--- javascript.js
+++ javascript.js
@@ -2,17 +2,17 @@
 // lấy chiều dài màn hình thực tế
 
 
-function setPlacehoder(x){
-    if (x.matches) { // If media query matches
-        $('#offer-input').attr('placeholder','Enter email...');
-      }
-    console.log(x);
+// function setPlacehoder(x){
+//     if (x.matches) { // If media query matches
+//         $('#offer-input').attr('placeholder','Enter email...');
+//       }
+//     console.log(x);
 
     
-}
-var x = window.matchMedia("(max-width: 416px)")
-setPlacehoder(x) // Call listener function at run time
-x.addListener(setPlacehoder) // Attach listener function on state changes
+// }
+// var x = window.matchMedia("(max-width: 416px)")
+// setPlacehoder(x) // Call listener function at run time
+// x.addListener(setPlacehoder) // Attach listener function on state changes
 
 
 
